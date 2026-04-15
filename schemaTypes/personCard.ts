5c9149dc7a36c535524d1c279dd6e4345fd40abc
Peter Swartwout <pswartwout@gmail.com>
2026-04-15 13:27:15 -0400

    Adds validation and hard limit for the bio section.

--- schemaTypes/personCard.ts
+++ schemaTypes/personCard.ts
@@ -7,7 +7,12 @@ export const personCard = defineType({
   fields: [
     defineField({ name: 'name', type: 'string' }),
     defineField({ name: 'profilePic', type: 'image', options: { hotspot: true } }),
-    defineField({ name: 'bio', type: 'text' }),
+    defineField({
+      name: 'bio',
+      title: 'Bio (200 characters max)',
+      type: 'text',
+      validation: (Rule) => Rule.max(200).error('Bio should be 200 characters or less'),
+    }),
   ]
 })
 
